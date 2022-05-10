ca8327938a7f177cfc00832e4601510dd54c2772
Matan <matanbitton2@gmail.com>
2022-05-10 19:20:12 +0300

    add buttons eventlisteners change homepage content

--- src/index.js
+++ src/index.js
@@ -4,22 +4,37 @@ import renderWineMenu from "./winemenu";
 import renderContactPage from "./contactpage";
 
 // creating elements
-const homePage = renderHomePage();
+const contentDisplayed = document.querySelector("#content");
+let homePage = renderHomePage();
 const pizzaMenu = renderPizzaMenu();
 const wineMenu = renderWineMenu();
 const contact = renderContactPage();
 const header = document.createElement("header");
 const buttonsContainer = menuButtons();
-
 const homeBtn = buttonsContainer.querySelector(".home-btn");
 const pizzaBtn = buttonsContainer.querySelector(".pizza-btn");
 const wineBtn = buttonsContainer.querySelector(".wine-btn");
 const contactBtn = buttonsContainer.querySelector(".contact-btn");
 
-header.className = "header";
-
 header.append(homeBtn, pizzaBtn, wineBtn, contactBtn);
+contentDisplayed.append(header, homePage);
 
-const content = document.querySelector("#content");
+//event listeners
+homeBtn.addEventListener("click", () => {
+  homePage.innerHTML = "";
+  homePage.append(renderHomePage());
+});
+pizzaBtn.addEventListener("click", () => {
+  homePage.innerHTML = "";
+  homePage.append(pizzaMenu);
+});
+wineBtn.addEventListener("click", () => {
+  homePage.innerHTML = "";
+  homePage.append(wineMenu);
+});
+contactBtn.addEventListener("click", () => {
+  homePage.innerHTML = "";
+  homePage.append(contact);
+});
 
-content.append(header, homePage);
+header.className = "header";
