7c8baad7ccfdb4c826809ee4c9faec99e6af9e70
Matan <matanbitton2@gmail.com>
2022-05-16 19:31:59 +0300

    add wine menu UI

--- src/winemenu.js
+++ src/winemenu.js
@@ -1,16 +1,20 @@
 export default function renderWineMenu() {
-  let menuContainer;
-  for (let i = 0; i <= 6; i++) {
-    menuContainer = document.createElement("div");
-    const menuItem = document.createElement("div");
-    const menuItemTitle = document.createElement("h4");
-    const menuItemPrice = document.createElement("h4");
+  const menuTitle = document.createElement("h3");
+  menuTitle.className = "wine-menu-title";
+  menuTitle.innerText = "Wine Menu";
+  let menuContainer = document.createElement("div");
+  menuContainer.className = "wine-menu-container";
+  menuContainer.append(menuTitle);
+  for (let i = 0; i <= 5; i++) {
+    let menuItem = document.createElement("div");
+    let menuItemTitle = document.createElement("h4");
+    let menuItemPrice = document.createElement("h4");
 
-    menuItem.className = "wine menu-item";
-    menuItemTitle.className = "wine menu-item-title";
-    menuItemPrice.className = "wine menu-item-price";
+    menuItem.className = "wine-menu-item";
+    menuItemTitle.className = "wine-menu-item-title";
+    menuItemPrice.className = "wine-menu-item-price";
 
-    menuItemTitle.innerText = "Wine";
+    menuItemTitle.innerText = "Wine Item";
     menuItemPrice.innerText = "$14.99";
     menuContainer.append(menuItem);
     menuItem.append(menuItemTitle, menuItemPrice);
